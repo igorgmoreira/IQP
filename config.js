2743363f8017eb46dca676c982e513e9e6f61317
igorgmoreira <igor.moreira@cobli.co>
2026-05-14 09:54:56 -0300

    Update config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// Arquivo: config.js
+// Aqui ficam todas as chaves e nomes das tabelas do Supabase
+
+const CONFIG = {
+    SUPABASE_URL: 'https://myulndnkmzthjlrmyfnk.supabase.co',
+    SUPABASE_ANON_KEY: 'sb_publishable_R0Bik6nRRPuTjNauBdTo7A_Ey-5CbdW',
+    
+    // Configurações do seu banco de dados
+    TABELA_USUARIOS: 'login_prestadores_iqp',
+    COLUNA_EMAIL: 'email',
+    COLUNA_SENHA: 'password',
+    COLUNA_NOME_PRESTADOR: 'prestador'
+};
